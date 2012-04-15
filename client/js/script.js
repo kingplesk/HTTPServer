43bbf7c7b482a760ec82dbfb02700c2caa02ee07
Andreas Eri <andreas.eri@e-ident.de>
2012-04-15 23:35:32 +0200

    rewrite notify as trayicon, signals with slots as private members, new dummy output for trayicons (ae)

--- client/js/script.js
+++ client/js/script.js
@@ -6,6 +6,15 @@ var Util = (function() {
         },
         "getUniqueId" : function() {
             return (new Date()).getTime() + "." + (Math.random() * 10000 + 10000);
+        },
+        "hide": function(e) {
+            e.style.display = "none";
+        },
+        "show": function(e) {
+            e.style.display = "";
+        },
+        "html": function(e, content) {
+            e.innerHTML = content;
         }
     };
 
@@ -59,36 +68,46 @@ var Ajax = (function() {
 })()
 
 var Signal = function() {
-    this.slots = {};
-    this.add = function(slot, context) {
+    var slots = {}
+
+    this.connect = function(slot, context) {
         context = context || null;
         var contextId = context + "", slotId = slot + "";
 
-        if (!this.slots[contextId]) {
+        if (!slots[contextId]) {
             var newSlots = {};
             newSlots[slotId] = {"slot": slot, "context": context};
-            this.slots[contextId] = newSlots;
+            slots[contextId] = newSlots;
         }
         else {
-            this.slots[contextId][slotId] = {"slot": slot, "context": context};
+            slots[contextId][slotId] = {"slot": slot, "context": context};
         }
     };
-};
 
-Signal.prototype = {
-    connect : function(slot, context) {
-        this.add(slot, context);
-    },
-    disconnect: function(context, slot) {
+    this.disconnect = function(context, slot) {
+        context = context || null;
+        var contextId = context + "", slotId = slot + "";
 
-    },
-    emit : function(data) {
-        for (var i in this.slots) {
-            for (var j in this.slots[i]) {
-                this.slots[i][j].slot.call(this.slots[i][j].context, data);
+        if (!slot) {
+            slots[contextId] = null;
+            return;
+        }
+
+        for (var slotIdx in slots[contextId]) {
+            if (slotIdx === slotId) {
+                slots[contextId][slotIdx] = null;
             }
         }
-    }
+    };
+
+    this.emit = function(data) {
+        for (var i in slots) {
+            for (var j in slots[i]) {
+                slots[i][j].slot.call(slots[i][j].context, data);
+            }
+        }
+    };
+
 };
 
 var Comet = (function(Ajax, Util, Signal) {
@@ -112,7 +131,6 @@ var Comet = (function(Ajax, Util, Signal) {
         error: function(statusCode) { console.log('Error: ' + statusCode); }
     };
 
-
     var start = function(url) {
         if (isStarted) return;
         isStarted = true;
@@ -121,12 +139,15 @@ var Comet = (function(Ajax, Util, Signal) {
 
     // BEGIN: TestTimer for Signal implmentation
     var timer = function() {
+        // 3 - 7 sec.
+        var timeout = (parseInt((Math.random() * 5), 10) + 3) * 1000;
+        console.log("Timeout:", timeout);
+
         setTimeout(function() {
             var id = parseInt((Math.random() * 7), 10);
             instance["test" + id] && instance["test" + id].emit("test" + id);
-
             timer();
-        }, 5000);
+        }, timeout);
     };
 
     timer();
@@ -155,25 +176,69 @@ var Comet = (function(Ajax, Util, Signal) {
 
 })(Ajax, Util, Signal);
 
-var Notify = (function(Util, Comet) {
+var TrayIcon = (function(Util, Comet) {
+    var instance;
     var timer = null, timeout = 5000;
-    var show = function(notifier) {
-        next(timeout);
-    };
+    var domContainer = Util.$id("TrayIcon"),
+        domClose = Util.$id("TrayIconClose"),
+        domPluginContainer = Util.$id("TrayIconPluginContainer");
 
-    var next = function() {
+    var show = function(content) {
+        reset();
+
+        Util.html(domPluginContainer, content);
+        Util.show(domContainer);
+
         timer = setTimeout(function() {
-            //hide content
+            hide();
         }, timeout);
     };
 
-    Comet.test0.connect(console.log);
-    Comet.test1.connect(console.log);
-    Comet.test2.connect(console.log);
-    Comet.test3.connect(console.log);
-    Comet.test4.connect(console.log);
-    Comet.test5.connect(console.log);
-    Comet.test5.connect(console.info);
-    Comet.test6.connect(console.log);
+    var reset = function() {
+        if (timer) clearTimeout(timer);
+        timer = null;
+    }
+
+    var hide = function() {
+        Util.hide(domContainer);
+        reset();
+    }
+
+    var close = function(e) {
+        e.preventDefault;
+        hide();
+    }
+
+    TrayIcon = function TrayIcon() {
+        if (instance) {
+            return instance;
+        }
+
+        instance = this;
+
+        this.show = show;
+    };
+
+    return new TrayIcon();
 
 })(Util, Comet);
+
+
+
+Comet.test0.connect(TrayIcon.show);
+Comet.test0.connect(console.log);
+Comet.test1.connect(console.log);
+Comet.test1.connect(TrayIcon.show);
+Comet.test2.connect(console.log);
+Comet.test2.connect(TrayIcon.show);
+Comet.test3.connect(console.log);
+Comet.test3.connect(TrayIcon.show);
+Comet.test4.connect(console.log);
+Comet.test4.connect(TrayIcon.show);
+Comet.test5.connect(console.log);
+Comet.test5.connect(console.info);
+Comet.test5.connect(TrayIcon.show);
+Comet.test6.connect(console.log);
+Comet.test6.connect(TrayIcon.show);
+
+
